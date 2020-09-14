f278ae3fdad32762c3dc6641af6d502e80d4fa52
arttujo <arttujo@gmail.com>
2020-09-14 13:14:03 +0300

    lisää osa4

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -4,26 +4,11 @@ const logger = require('./utils/logger')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs',blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info("Server running on Port",config.PORT)
